df36453001aaadce121ed831377e3a0a7eac8eca
renato.guimaraes <renato.guimaraes@novitatus.com>
2018-03-23 00:15:13 -0300

    poc

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var fs = require('fs');
-var Mustache = require('mustache');
-var pdf = require('html-pdf');
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
+const fs = require('fs');
+const Mustache = require('mustache');
+const pdf = require('html-pdf');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/pdf', (req, res) => {
 
@@ -17,16 +17,16 @@ app.get('/pdf', (req, res) => {
   };
 
   const readHtml = (err, data) => {
-    var html = Mustache.render(data, { name: 'Renato' })
+    const html = Mustache.render(data, { name: 'Renato' })
     pdf.create(html, { format: 'A4' }).toStream(writePdf);
   };
 
   fs.readFile('./templates/poc.html', 'utf8', readHtml);
 });
 
-var server = app.listen(8081, () => {
-  var host = server.address().address
-  var port = server.address().port
+const server = app.listen(8081, () => {
+  const host = server.address().address
+  const port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
 })
 
