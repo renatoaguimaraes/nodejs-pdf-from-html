a637f4a2ed55fa9004fa87dc5dfc6c802846eca7
renato.guimaraes <renato.guimaraes@novitatus.com>
2018-03-22 23:47:30 -0300

    poc

---
+++ index.js
@@ -0,0 +1,30 @@
+var PDFDocument, doc;
+var fs = require('fs');
+var Mustache = require('mustache');
+var pdf = require('html-pdf');
+
+var express = require('express');
+var app = express();
+
+app.get('/pdf', (req, res) => {
+
+  const writePdf = (err, stream) => {    
+    res.contentType("application/pdf");    
+    pdfStream.pipe(res)    
+    pdfStream.on('end', () => res.end());
+  };
+
+  const readHtml = (err, data) => {
+    var html = Mustache.render(data, { name: 'Renato' })
+    pdf.create(html, { format: 'A4' }).toStream(writePdf);
+  };
+
+  fs.readFile('./template.html', 'utf8', readHtml);
+});
+
+var server = app.listen(8081, () => {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+})
+
